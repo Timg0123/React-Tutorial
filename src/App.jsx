20dd391229d00fd36706dd2b726264caf1fcde50
Timg0123 <73588975+Timg0123@users.noreply.github.com>
2022-12-22 14:02:27 +0100

    added custome Hook and started to make Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
       </div>
-    </div>
+    </Router>
   ) 
 }
-
-export default App;
