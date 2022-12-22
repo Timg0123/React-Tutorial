a51aa2432cb0d234f9527a2f090e050684ba2499
Timg0123 <73588975+Timg0123@users.noreply.github.com>
2022-12-22 15:05:08 +0100

    added comments and updated useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,27 +1,42 @@
 import { useState, useEffect } from 'react'
 
 const useFetch = (url) => {
+    // defining state variables
     const [data, setData] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+
+        fetch(url, { signal: abortCont.signal })
+
+            // checking if response has an error and making the json to js
             .then(res => {
-                if(!res.ok) {
+                if (!res.ok) {
                     throw Error('could not fetch the data for that resource')
                 }
                 return res.json();
             })
+
+            //  If this is executed we set loading to false and error to false because everything works
             .then(data => {
                 setData(data)
                 setIsLoading(false)
                 setError(null)
             })
+
+            // if the first block gave an error, we catch it here and log it to the console
             .catch((err) => {
-                setIsLoading(false)
-                setError(err.message)
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
+                    setIsLoading(false)
+                    setError(err.message) // to show the error on the website
+                }
             })
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isLoading, error }
