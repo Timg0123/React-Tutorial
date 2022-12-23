76d37eb91908c376087c270d94bed3532865a0d5
Timg0123 <73588975+Timg0123@users.noreply.github.com>
2022-12-23 12:49:36 +0100

    added 404 page and fixed create and delete button

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,7 +1,32 @@
+import { useHistory, useParams } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
+import BlogList from './BlogList';
+
 const BlogDetails = () => {
+    const { id } = useParams()
+    const { data: blog, isLoading, error } = useFetch(`http://localhost:8000/blogs/${id}`)
+    const history = useHistory()
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/')
+        })
+    }
+
     return (
         <div className="blog-details">
-            <h2>Blog details</h2>
+            { isLoading && <div>Loading...</div> }
+            { error && <div>{ error }</div> }
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
+                </article>
+            )}
         </div>
     );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="home">
             { error && <div>{ error }</div> }
             { isLoading && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+            {blogs && <BlogList blogs={blogs} title={Object.keys(blogs).length === 0 ? 'Currently no blogs' : 'All blogs'} />}
         </div>
     );
 }
